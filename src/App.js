26e3e0228e6e7bf9014fd395b8ef2af49b42a6bd
Psychovisual <35844754+Psychovisual@users.noreply.github.com>
2019-12-02 10:07:04 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Navigation';
+import Content from './Content';
+
+function App() {
+  return (
+    <div className="fill-window">
+      <Navigation />
+      
+      
+
+    </div>
+    
+  );
+}
+
+export default App;
