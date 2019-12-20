575bf3460ad20b55b6d97a14592d83f850193e99
Touze-Menko Timothy <apxh@mediait.ch>
2019-12-20 14:21:22 +0100

    Updates

---
+++ precache-manifest.b02b09265f0c7abf24c4bbc3f46c8f78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2366a66e5dd509dc6013993016a4cca2",
+    "url": "/react-ness-mp/index.html"
+  },
+  {
+    "revision": "b800642d92f0cfd36090",
+    "url": "/react-ness-mp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "930fc647acf2f803adb5",
+    "url": "/react-ness-mp/static/css/main.dcddc07f.chunk.css"
+  },
+  {
+    "revision": "b800642d92f0cfd36090",
+    "url": "/react-ness-mp/static/js/2.f708ea38.chunk.js"
+  },
+  {
+    "revision": "930fc647acf2f803adb5",
+    "url": "/react-ness-mp/static/js/main.3012bb2c.chunk.js"
+  },
+  {
+    "revision": "6424799b0218ffd57786",
+    "url": "/react-ness-mp/static/js/runtime-main.2f3fc12a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-ness-mp/static/media/logo.25bf045c.svg"
+  }
+]);
